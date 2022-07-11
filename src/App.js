1e7088f8ba49f7fce558cac8aead9a7026418b8b
VMLujanJr <vmlujanjr@outlook.com>
2022-07-11 12:06:53 -0500

    useState and useEffect on Navigation & App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+/* So what exactly are Hooks? Hooks are just JavaScript functions that follow two rules: */
+/* The first rule is "Only call Hooks from React functions." This should be easy to remember, because you often use Hooks to affect the state of a component in some way and trigger the component to render. */
+/* The second rule is "Only call Hooks at the top level". This means that you cannot use them inside for loops, nested functions within your React component, or conditionals. For organizational purposes and to ensure that you're following these rules, you may want to use Hooks towards the top of your React component. */
+import React, { useState } from 'react';
+import Gallery from './components/Gallery';
 import Navigation from './components/Navigation';
 import About from './components/About';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    {
+      name: 'portraits',
+      description: 'Portraits of people in my life',
+    },
+    {
+      name: 'food',
+      description: 'Delicious delicacies',
+    },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Navigation></Navigation>
+      <Navigation
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Navigation>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
   );
-}
+};
 
 export default App;
